8d61672189f5482a38d9e888db3327b1326dba33
MaolyDevTech <maolydevtech@gmail.com>
2023-09-21 19:48:29 -0300

    refector index, routes and controller

--- controller/TaksController.js
+++ controller/TaksController.js
@@ -3,7 +3,7 @@ const Task = require("../models/Task");
 const getAllTasks = async (req, res) => {
   try {
     const tasksList = await Task.find();
-    return res.render("index", { tasksList });
+    return res.render("index", { tasksList, task: null, taskDelete: null });
   } catch (err) {
     res.status(500).send({ error: err.message });
   }
@@ -24,7 +24,35 @@ const createTask = async (req, res) => {
   }
 };
 
+const getById = async (req, res) => {
+  try {
+    const { task, taskDelete } = await Task.findOne({ _id: req.params.id });
+    const tasksList = await Task.find();
+    if (res.params.method == Update) {
+      res.render("index", { task, taskDelete: null, tasksList });
+    } else {
+      res.render("index", { task: null, taskDelete, tasksList });
+    }
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+};
+
+const updateOneTask = async (req, res) => {
+  try {
+    const task = req.body;
+    await Task.updateOne({ _id: req.params.id }, task);
+    // Adicione a atualização da data ao objeto task
+    task.date = new Date(); // Define a data atual como data da tarefa
+    res.redirect("/");
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+};
+
 module.exports = {
   getAllTasks,
   createTask,
+  getById,
+  updateOneTask,
 };
--- routes/routes.js
+++ routes/routes.js
@@ -3,5 +3,7 @@ const TaskController = require("../controller/TaksController");
 
 routes.get("/", TaskController.getAllTasks);
 routes.post("/create", TaskController.createTask);
+routes.get("/getById/:id/:method", TaskController.getById); 
+routes.post("/updateOne/:id", TaskController.updateOneTask)
 
 module.exports = routes;
